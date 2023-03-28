42d465300d7556e8bb9f6fdca78ff0e8d9e72de1
sebastian jimenez <sebasjiemenez1121@gmail.com>
2023-03-28 09:07:11 -0500

    primer commit

---
+++ ciclos/potencia.js
@@ -0,0 +1,9 @@
+let b = parseInt(prompt("ingrese el numero que dese potenciar el nuemro a"));
+let a = parseInt(prompt("ingrese el numero a"));
+let i = 1; 
+    while (i <= b) {
+        
+        i++
+    }
+
+    document.write("el resultado de la potenciacion es" + a)
